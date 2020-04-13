ff87f005eaf439acf6ac3a4bd93b994976474252
ZJN <810863273@qq.com>
2020-04-14 01:02:57 +0800

    Front page update

--- src/main/java/com/zjn/myblog/controller/IndexController.java
+++ src/main/java/com/zjn/myblog/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,7 +15,7 @@ public class IndexController {
 
     @Autowired
     private BlogService blogService;
-    
+
     @GetMapping("/")
     public String index(@RequestParam(defaultValue = "1") Integer page,
                         Model model) {
@@ -24,4 +25,10 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/blog/{id}")
+    public String blog(@PathVariable Long id, Model model) {
+        model.addAttribute("blog", blogService.getBlog(id));
+        return "blog";
+
+    }
 }
